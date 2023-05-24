91319120748dcbdd71c5f120f70fa0b59c305a33
ubi2000 <obaidnabi1991@gmail.com>
2023-05-24 20:58:37 +0530

    f

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+import ContactCard from "./ContactCard";
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "obaid",
+      email: "obaid@gmail.com",
+    },
+    {
+      id: "2",
+      name: "Lone",
+      email: "lone@gmail.com",
+    }
+  ];
   return (
-    <div>
-    <h1>hello world</h1>
+    <div className="ui container">
+      <Header />
+      <AddContact />
+      <ContactList contacts={contacts}/>
+     
     </div>
   );
 }
